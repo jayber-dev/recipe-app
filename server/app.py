8aa19926f1ddcac466fbb29457d2635883c3d52c
jay ber <jayber1@gmail.com>
2022-11-29 13:51:47 +0200

    pony orm added

--- server/app.py
+++ server/app.py
@@ -5,32 +5,32 @@ from flask_cors import CORS
 from pony.orm import *
 
 
+
 app = Flask(__name__)
 CORS(app=app)
 db = Database()
 
-
-
+class Person(db.Entity):
+    name = Required(str)
+    age = Required(int)
 
 @app.route('/login', methods=['GET','POST'])
 def login ():
-    if(request.method == 'GET'):
-        # print(requests)
-        return jsonify([{'nana':'lala'},2,3])
-    elif(request.method == 'POST'):
+    if(request.method == 'POST'):
         data = request.get_json()
         print(data['name'])
         if(data['name'] == 'jayber1@gmail.com'):
             return jsonify({'data':'true'})
         else:
             return jsonify({'data':'false'})  
-        # return data
+    return jsonify([{'nana':'lala'},2,3])
+    
 
 @app.route('/logout', methods=['GET','POST'])
 def logout():
     return jsonify({'logout':'true'})
     
 
-
 if __name__ == "__main__":
+    
     app.run(debug=True,host='127.0.0.1',port='5001')
