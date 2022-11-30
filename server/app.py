89de3fadb95d79fb5b58ba9ae4b554f293ddb635
jay ber <jayber1@gmail.com>
2022-11-30 14:17:31 +0200

    simple validation added + token added

--- server/app.py
+++ server/app.py
@@ -4,39 +4,42 @@ import mysql.connector
 from flask_cors import CORS
 # from pony.orm import *
 import db.entities as entity
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
 CORS(app=app)
-
-# mydb = mysql.connector.connect(
-#     host='31.170.164.51',
-#     user='u889934763_p00nani',
-#     password='Pp0526767682!',
-#     database='u889934763_recipeUsers',  
-# )
-
-# print(mydb)
-# class Person(db.Entity):
-#     user = Required(str)
-#     passeord = Required(int)
-
-# @db_session
-# def insert():
-#     pass
    
 entity.db.bind(provider='mysql',host='31.170.164.51', user='u889934763_p00nani', passwd='Pp0526767682!', db='u889934763_recipeUsers')
 entity.db.generate_mapping(create_tables=True)
 
+# entity.insert('jayber1@gmail.com','Pp304682685!')
+user_list = entity.retrive_user_list()
+for i in user_list:
+    print(i.email, i.password)
+# print(user_list[0].email)
+
 @app.route('/login', methods=['GET','POST'])
 def login ():
-    if(request.method == 'POST'):
-        data = request.get_json()
-        print(data['name'])
-        if(data['name'] == 'jayber1@gmail.com'):
-            return jsonify({'data':'true'})
+    user = request.get_json()
+    try:
+        user_data = entity.retrive_user(user['name'])
+        if(request.method == 'POST'):
+            data = request.get_json()
+            before_hash = 'nana'
+            hashed_pass = generate_password_hash(password=before_hash,method='pbkdf2:sha256:20000')
+            print(hashed_pass)
+            print(check_password_hash(hashed_pass,'nana'))
+            print(data['name'])
+        if(data['name'] == user_data.email):
+            return jsonify({'data':check_password_hash(hashed_pass,'nana'),'hashed_pass':hashed_pass})
         else:
             return jsonify({'data':'false'})  
+    except:
+        pass
+    
+    # print(user_data.email,user_data.password)
+    
     return jsonify([{'nana':'lala'},2,3])
     
 
--- server/db/entities.py
+++ server/db/entities.py
@@ -5,16 +5,22 @@ print('hello from entities')
 db = Database()
 
 @db_session
-def insert():
-    pass
-
-
-class Person(db.Entity):
-    user = Required(str, unique=True)
-    passeord = Required(int)
+def insert(email,password):
+    insert_data = Users(email=email,password=password)
+    # commit()
     
-class User(db.Entity):
+@db_session
+def retrive_user(email):
+    return Users.get(email=email)
+
+@db_session
+def retrive_user_list():
+    return select(p for p in Users)[:]
+
+
+class Users(db.Entity):
     email = Required(str,unique=True)
     password = Required(str)
+    token = Optional(str)
     
 
