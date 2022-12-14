49dff893ead0b7b5d113812ec8718dfd21834edf
jayber <jayber1@gmail.com>
2022-12-14 11:07:24 +0200

    add recipe added and pic view fixed

--- src/app/components/add-recipe/add-recipe.component.ts
+++ src/app/components/add-recipe/add-recipe.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import { FormGroup,FormBuilder,FormArray, } from '@angular/forms';
+import { Component, SecurityContext } from '@angular/core';
+import { FormGroup,FormBuilder,FormArray, Validators, } from '@angular/forms';
 import { Pipe, PipeTransform } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 
 @Component({
@@ -12,19 +12,38 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class AddRecipeComponent {
 
   addRecipe: FormGroup
+  SecurityContext: any;
+  url:SafeUrl;
+  ingredients:string[] = [];
 
   constructor(fb:FormBuilder, public Sanitaizer:DomSanitizer){
     this.addRecipe = fb.group({
       title: '',
       cookingTime: '',
       primaryImage: '',
-      ingredient:'',
+      ingredient:['', Validators.required],
       steps: '',
     })
   }
 
+  addIngredient(){
+    this.ingredients.push(this.addRecipe.get('ingredient').value)
+    this.addRecipe.controls['ingredient'].setValue('')
+    this.addRecipe.controls['ingredient'].markAsUntouched()
+  }
+
+  readUrl(event:any) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+  
+      reader.onload = (event: ProgressEvent) => {
+        this.url = (<FileReader>event.target).result;
+      }
+  
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
   onSubmit(){
     console.log('thank you for your submission');
-    
   }
 }
