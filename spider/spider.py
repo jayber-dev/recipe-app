3be0376f472053b37a6cf22497fb97ea40009011
jayber <jayber1@gmail.com>
2023-01-16 12:28:15 +0200

    file handling and spider

--- spider/spider.py
+++ spider/spider.py
@@ -1,11 +1,44 @@
+
 import re
 from playwright.sync_api import sync_playwright
-
+from pony.orm import *
 import requests
 import json
 
+db = Database()
+
+db.bind(provider='sqlite', filename='../server/db.sqlite')
 
 
+class Users(db.Entity):
+    firstName = Required(str)
+    lastName = Required(str)
+    country = Optional(str)
+    email = Required(str, unique=True)
+    password = Required(str)
+    token = Optional(str)
+    imgName = Optional(str)
+    recipes = Set('Recipes')
+
+
+class Recipes(db.Entity):
+    user = Required(Users)
+    recipe_name = Required(str)
+    cooking_time = Required(int)
+    preperation_time = Required(int)
+    primary_image = Required(str)
+    ingredients = Required(LongStr)
+    cooking_steps = Required(LongStr)
+
+
+db.generate_mapping()
+
+@db_session
+def get_data():
+    data = list(Recipes.select())
+    for i in data:
+        print(i.ingredients)
+    
 
 # pw = sync_playwright().start()
 
@@ -25,3 +58,4 @@ json_data = (json.loads(data.text))
 for i in json_data['data']:
     print(f"name: {i['name']} ----------------|--------------- price: {i['price']['price']}")
 
+get_data()
